805a85c4ea242265af63f0234047282c83701a16
Sounak Pradhan <sounak.98@gmail.com>
2019-09-12 12:50:29 +0530

    Updates

---
+++ precache-manifest.2134096bede7250dfe2ead23ed6ec747.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0131cec222d299d3b2890ffb920e41c",
+    "url": "/easy-token-launcher/index.html"
+  },
+  {
+    "revision": "eaabeafbc033fda2e0e7",
+    "url": "/easy-token-launcher/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "64fd6b19038bf153f11c",
+    "url": "/easy-token-launcher/static/js/2.341a6927.chunk.js"
+  },
+  {
+    "revision": "eaabeafbc033fda2e0e7",
+    "url": "/easy-token-launcher/static/js/main.f75fcdc4.chunk.js"
+  },
+  {
+    "revision": "72d367c2b96b63cb9397",
+    "url": "/easy-token-launcher/static/js/runtime~main.7af51302.js"
+  }
+]);
